1bf8790c858057b34edb10b8b45efb9113bd6ee7
harld-x <danilyudin723@gmail.com>
2021-08-27 15:59:44 +0700

    calendar

--- src/pages/Consultation.js
+++ src/pages/Consultation.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import { NavBar } from '../components';
 const Consultation = () => {
   useEffect(() => {
     const head = document.querySelector('head');
@@ -10,25 +10,28 @@ const Consultation = () => {
   });
 
   return (
-    <div className="App" style={{ textAlign: 'center' }}>
-      <h1 className="title" style={{ fontSize: 30, fontWeight: 300 }}>
-        Thank you!
-      </h1>
-      <p>
-        You've taken your first step towards a brighter future. Select a day and time to speak with
-        one of our knowleadeable team members
-      </p>
-      <div>
-        <Link to="/" className="arrow" style={{ fontSize: 20, color: '#000' }}>
-          <span className="svg-arrow" />
-          Back to Home
-        </Link>
+    <>
+      <NavBar />
+      <div className="App" style={{ textAlign: 'center' }}>
+        <h1 className="title" style={{ fontSize: 30, fontWeight: 300 }}>
+          Thank you!
+        </h1>
+        <p>
+          You've taken your first step towards a brighter future. Select a day and time to speak
+          with one of our knowleadeable team members
+        </p>
+        <div>
+          <Link to="/" className="arrow" style={{ fontSize: 20, color: '#000' }}>
+            <span className="svg-arrow" />
+            Back to Home
+          </Link>
+        </div>
+        <div
+          className="calendly-inline-widget"
+          data-url="https://calendly.com/a1creditpartners"
+          style={{ minWidth: '320px', height: '630px' }}></div>
       </div>
-      <div
-        className="calendly-inline-widget"
-        data-url="https://calendly.com/a1creditpartners"
-        style={{ minWidth: '320px', height: '630px' }}></div>
-    </div>
+    </>
   );
 };
 
