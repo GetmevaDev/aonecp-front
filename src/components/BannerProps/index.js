a7cee498a43b067632847c1ac87081a66cdad4d1
harld-x <danilyudin723@gmail.com>
2021-08-31 20:48:33 +0700

    form

--- src/components/BannerProps/index.js
+++ src/components/BannerProps/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './styles.module.css';
-import cn from 'classnames';
 
 const BannerProps = ({ title, imgUrl, titleColor }) => {
   return (
     <div className={styles.pricing}>
       <div>
         <img className={styles.imgProp} src={imgUrl} alt="" />
-        <h1 className={cn(styles.title)}>{title}</h1>
+        <h1 className={styles.title}>{title}</h1>
       </div>
     </div>
   );
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,76 +1,92 @@
 import React from 'react';
 import styles from './styled.module.css';
+import emailjs from 'emailjs-com';
 
 const Form = () => {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_mqtpv9t', 'template_hxleb5a', e.target, 'user_iw2a3XOS7O7HrGbR8S31M')
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+  }
+
   return (
     <div className="container">
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={sendEmail}>
         <ul>
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}> First Name</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="name" type="text" />
             </label>
           </li>
 
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}> Last Name</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="lastname" type="text" />
             </label>
           </li>
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}>Company Name</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="company" type="text" />
             </label>
           </li>
 
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}> Address</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="address" type="text" />
             </label>
           </li>
 
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}>City</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="city" type="text" />
             </label>
           </li>
 
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}> State</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="state" type="text" />
             </label>
           </li>
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}> ZIP code</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="zipcode" type="text" />
             </label>
           </li>
 
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}> Phone</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="phone" type="text" />
             </label>
           </li>
 
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}>Email</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="user_email" type="email" />
             </label>
           </li>
 
           <li>
             <label className={styles.name}>
               <h5 className={styles.textInput}> Promotional Methods</h5>
-              <input className={styles.inputForm} name="username" type="text" />
+              <input className={styles.inputForm} name="promotionals" type="text" />
             </label>
           </li>
           <li>
@@ -79,13 +95,13 @@ const Form = () => {
               <input
                 className={styles.inputForm}
                 style={{ width: '225%' }}
-                name="username"
+                name="comment"
                 type="text"
               />
             </label>
           </li>
         </ul>
-        <button className={styles.btnSend}>Send</button>
+        <input className={styles.btnSend} type="submit" value="Send" />
       </form>
     </div>
   );
