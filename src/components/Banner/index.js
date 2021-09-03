7b4a491c3dc674122a574532c4be08443b177931
harld-x <danilyudin723@gmail.com>
2021-09-03 15:27:48 +0700

    banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -9,26 +9,28 @@ import Pupup from '../Popup';
 const Banner = () => {
   return (
     <div className={styles.banner}>
-      <div className={styles.content}>
-        <div className={styles.logoService}></div>
-        <h1 className={styles.title}>Restoring lives the easy way</h1>
-        <p className={styles.description}>Free for the first 30 days</p>
-        <div className={styles.buttons}>
-          <Pupup />
-          <button className="btnYellow">
-            <Link to="/thankyou">Free Consultation</Link>
-          </button>
-        </div>
-        <a href="tel:516-828-2338">
-          <div className={styles.call}>
-            <img className={styles.callSvg} src={SignSvgPhone} alt="" />
-
-            <div className={styles.tel}>
-              <span className={styles.callNow}>Call now</span> <br />
-              <b>800-974-7917</b>
-            </div>
+      <div className={styles.bannerInner}>
+        <div className={styles.content}>
+          <div className={styles.logoService}></div>
+          <h1 className={styles.title}>Restoring lives the easy way</h1>
+          <p className={styles.description}>Free for the first 30 days</p>
+          <div className={styles.buttons}>
+            <Pupup />
+            <button className="btnYellow">
+              <Link to="/thankyou">Free Consultation</Link>
+            </button>
           </div>
-        </a>
+          <a href="tel:516-828-2338">
+            <div className={styles.call}>
+              <img className={styles.callSvg} src={SignSvgPhone} alt="" />
+
+              <div className={styles.tel}>
+                <span className={styles.callNow}>Call now</span> <br />
+                <b>800-974-7917</b>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
